e72f057476dd15781e270f41c0f8eff901162f53
qiyang <qiyang@sodalife.xyz>
2026-01-07 11:00:28 +0800

    chore: use filesystem manage the mod dependency

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-mod thread {
-    pub mod producer_consumer;
-    pub mod thread_scope;
-}
+mod thread;
 fn main() {
-    // thread scope process
-    println!("thread scope process");
-    thread::thread_scope::process();
-
-    // producer consumer process
-    println!("producer consumer process");
-    thread::producer_consumer::process();
+    thread::pipeline::process();
 }
