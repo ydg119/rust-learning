958a8c93ddb032917023a1cb31efc7f2e7359e05
qiyang <qiyang@sodalife.xyz>
2026-01-14 19:00:34 +0800

    feat: add reff

--- src/basic/mod.rs
+++ src/basic/mod.rs
@@ -1,3 +1,4 @@
 pub mod closure;
 pub mod impl_display;
 pub mod where_trait;
+pub mod reff;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
 mod basic;
-mod data_structures;
-mod thread;
-
-use data_structures::queue::Queue;
 
 fn main() {
-    let mut queue: Queue<u8> = Queue::new();
-    queue.enqueue(1);
-    queue.enqueue(9);
-    println!("queue: {:?}", queue);
-    println!("queue is empty? : {}", queue.is_empty());
-    queue.dequeue();
-    println!("queue: {:?}", queue);
-    println!("queue is empty? : {}", queue.is_empty());
-    queue.dequeue();
-    println!("queue: {:?}", queue);
-    println!("queue is empty? : {}", queue.is_empty());
+    basic::reff::test();
 }
